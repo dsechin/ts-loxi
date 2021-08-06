e010c002d8b7913675251d2b6f14758c19d765bf
dsechin <d.sechin@ndmsystems.com>
2021-08-06 20:25:23 +0300

    (task) main.ts: add basic prompt

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,46 @@
+#!/usr/bin/env node
 import * as fs from 'fs';
+import * as readline from 'readline';
+import yargs from 'yargs/yargs';
+import * as _ from 'lodash';
 
-console.log(
-  __dirname,
-  fs.readdirSync(__dirname),
-);
+const argv = yargs(process.argv.slice(2))
+  .usage('$0 [script]')
+  .options({
+    script: {type: 'string', demandOption: false},
+  })
+  .argv;
+
+console.log(argv);
+
+const runFile = (path: string) => {
+  if (!fs.existsSync(path) || !fs.statSync(path).isFile) {
+    console.log(`Passed script "${path}" does not exist or is not a file`);
+    process.exit(-1);
+  }
+};
+
+const runPrompt = () => {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  const readSingleLine = () => {
+    rl.question('> ', (answer) => {
+      if (answer === 'exit') {
+        return rl.close();
+      }
+
+      readSingleLine();
+    });
+  };
+
+  readSingleLine();
+};
+
+if (_.has(argv, 'script')) {
+  runFile(_.get(argv, 'script', ''));
+} else {
+  runPrompt();
+}
